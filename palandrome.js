93cd4016c3b7a9e8c9be3dd4bd78e4c84d2c05d2
unknown <ashishparate66@gmail.com>
2022-10-19 17:16:14 +0530

    added problems

---
+++ palandrome.js
@@ -0,0 +1,12 @@
+let str="racecar";
+let bag="";
+for(let i=n-1;i>=0;i++){
+    bag = bag+str[i];
+}
+if(str==bag)
+{
+    console.log("Yes");
+}
+else{
+    console.log("No");
+}
---
+++ prime.js
@@ -0,0 +1,13 @@
+let num=13;
+let count=0;
+for(let i=0;i<=num;i++){
+  if(i%num===0){
+    count++;
+  }
+}
+if(count!==2){
+  console.log("Yes it is Prime number");
+}
+else{
+  console.log("Not prime number");
+}
